09c3677a4f8b22902973b6a85689630144b08232
AyushRawat17-code <ayushrawat12321@gmail.com>
2025-11-10 20:15:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,39 +15,39 @@ document.addEventListener('keydown',function(event){
 
 function makeSound(key){
     switch (key) {
-        case "s":
+        case "w":
             let tom1 = new Audio('sounds/tom-1.mp3');
             tom1.play();
             
             break;
-        case "d":
+        case "a":
             let tom2 = new Audio('sounds/tom-2.mp3');
             tom2.play();
             break;
-        case "f":
+        case "s":
             let tom3 = new Audio('sounds/tom-3.mp3');
             tom3.play();
             
             break;
-        case "g":
+        case "d":
             let tom4 = new Audio('sounds/tom-4.mp3');
             tom4.play();
             
             break;
 
-        case "h":
+        case "j":
             let snare = new Audio('sounds/snare.mp3');
             snare.play();
             
             break;
 
-        case "j":
+        case "k":
             let crash = new Audio('sounds/crash.mp3');
             crash.play();
             
             break;
 
-        case "k":
+        case "l":
             let kickBass = new Audio('sounds/kick-bass.mp3');
             kickBass.play();
             
